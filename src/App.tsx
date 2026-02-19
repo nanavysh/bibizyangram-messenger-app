43e46f2a00010f668538268137dffe4aeb796432
Nanavysh <nanavysh@icloud.com>
2026-02-19 09:58:14 +0500

    feat: добавлена базовая структура и Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
+import { Sidebar } from "./features/sidebar/Sidebar"
 
+function App() {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Sidebar />
+            <div style={{padding: '20px'}}>
+                {/*<h2>Здесь будут чаты</h2>*/}
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/features/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { List, Avatar, Typography } from "antd"
+
+const { Text, Title } = Typography
+
+export function Sidebar() {
+    return (
+        <div>
+            <Title>BIBIZYAMGRAM</Title>
+        </div>
+    )
+}
